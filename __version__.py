17d635c0c85201c28f49e58409606333ce02531b
Joe Pitt <joe.pitt@joepitt.co.uk>
2024-02-26 22:05:58 +0000

    initial commit

---
+++ __version__.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2024 Joe Pitt
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+__title__ = "synapse"
+__description__ = "Python Synapse (Matrix) server Admin API module."
+__author__ = "Joe Pitt"
+__copyright__ = "Copyright 2023, Joe Pitt"
+__email__ = "Joe.Pitt@joepitt.co.uk"
+__license__ = "GPL-3.0-only"
+__maintainer__ = "Joe Pitt"
+__status__ = "Prototype"
+__version__ = "1.0.0"
