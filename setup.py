14a91c54cb9cc20902bb20e238f9a1cfb473b8cf
Joe Pitt <joe.pitt@joepitt.co.uk>
2024-02-27 19:56:10 +0000

    fix: add long description

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 """Setup file for pysynapse"""
 
+from os.path import abspath, dirname, join
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
+    readme_file = join(dirname(abspath(__file__)), "README.md")
+    with open(readme_file, "r") as f:
+        readme = f.read()
+
+    setup(long_description=readme, long_description_content_type="text/markdown")
