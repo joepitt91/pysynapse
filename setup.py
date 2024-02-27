9629015f68a8598b205dc2f464365f546749f6a5
Joe Pitt <joe.pitt@joepitt.co.uk>
2024-02-27 20:16:38 +0000

    fix: complete pyproject

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 """Setup file for pysynapse"""
 
-from os.path import abspath, dirname, join
 from setuptools import setup
 
 if __name__ == "__main__":
-    readme_file = join(dirname(abspath(__file__)), "README.md")
-    with open(readme_file, "r", encoding="utf-8") as f:
-        readme = f.read()
-
-    setup(long_description=readme, long_description_content_type="text/markdown")
+    setup()
